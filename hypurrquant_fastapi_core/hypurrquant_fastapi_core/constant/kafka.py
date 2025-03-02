08707a4f317a9323e1fa60167fa947167bf01536
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-03-02 14:25:00 +0900

    fix: 버전 변경

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/kafka.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/kafka.py
@@ -1,12 +1,19 @@
+from enum import Enum
 from hypurrquant_fastapi_core.constant.projects import PROJECT_NAME, SERVICES
 
-ACCOUNT_KAFKA_TOPIC = {
-    "DELETE_ACCOUNT": f"{PROJECT_NAME}.{SERVICES['account']}.account.delete",
-    "REBALANCE_ACCOUNT_CAHNGE": f"{PROJECT_NAME}.{SERVICES['account']}.rebalance.account.change",
-    "REBALACNE_ACCOUNT_REFRESH": f"{PROJECT_NAME}.{SERVICES['account']}.rebalance.account.refresh",
-    "SPOT_BALANCE_REFRESH": f"{PROJECT_NAME}.{SERVICES['account']}.spot.balance.refresh",
-}
 
-DATA_KAFKA_TOPIC = {
-    "SPOT_MARKET_DATA_MID_PRICE": f"{PROJECT_NAME}.{SERVICES['data']}.spotMarket.mid_price"
-}
+class AccountKafkaTopic(Enum):
+    DELETE_ACCOUNT = f"{PROJECT_NAME}.{SERVICES['account']}.account.delete"
+    REBALANCE_ACCOUNT_CHANGE = (
+        f"{PROJECT_NAME}.{SERVICES['account']}.rebalance.account.change"
+    )
+    REBALANCE_ACCOUNT_REFRESH = (
+        f"{PROJECT_NAME}.{SERVICES['account']}.rebalance.account.refresh"
+    )
+    SPOT_BALANCE_REFRESH = f"{PROJECT_NAME}.{SERVICES['account']}.spot.balance.refresh"
+
+
+class DataKafkaTopic(Enum):
+    SPOT_MARKET_DATA_MID_PRICE = (
+        f"{PROJECT_NAME}.{SERVICES['data']}.spotMarket.midPrice"
+    )
