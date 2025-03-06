0866b54824d3f00ba340cc51d7f4f24357a89390
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-03-07 02:09:14 +0900

    구조 변경

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/redis.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/redis.py
@@ -1,18 +1,27 @@
-from hypurrquant_fastapi_core.constant.projects import PROJECT_NAME, SERVICES
+from hypurrquant_fastapi_core.constant.projects import PROJECT_NAME, Service
 
-ACCOUNT_REDIS_KEY = {
-    "REFRESH_LOCK": f"{PROJECT_NAME}:{SERVICES['account']}:refresh_lock:{{public_key}}",
-    "BALANCE": f"{PROJECT_NAME}:{SERVICES['account']}:balance:{{ticker}}",
-}
+from enum import Enum
 
-DATA_REDIS_KEY = {
-    "ALL_MIDS": f"{PROJECT_NAME}:{SERVICES['data']}:all_mids",
-    "ALL_MIDS_COUNT": f"{PROJECT_NAME}:all_mids_count",
-    "PERP_CANDLE": f"{PROJECT_NAME}:{SERVICES['data']}:perp_candle",
-    "PERP_MOMENTUM": f"{PROJECT_NAME}:{SERVICES['data']}:perp_momentum",
-    "PERP_META": f"{PROJECT_NAME}:{SERVICES['data']}:perp_meta",
-    "PERP_MARKET_DATA": f"{PROJECT_NAME}:{SERVICES['data']}:perp_market_data",
-    "CANDLE": f"{PROJECT_NAME}:{SERVICES['data']}:candle",
-    "MOMENTUM": f"{PROJECT_NAME}:{SERVICES['data']}:momentum",
-    "MARKET_DATA": f"{PROJECT_NAME}:{SERVICES['data']}:market_data",
-}
+
+class AccountRedisKey(Enum):
+    REFRESH_LOCK = f"{PROJECT_NAME}:{Service.ACCOUNT.value}:refresh_lock:{{public_key}}"
+    BALANCE = f"{PROJECT_NAME}:{Service.ACCOUNT.value}:balance:{{ticker}}"
+
+
+class DataRedisKey(Enum):
+    ALL_MIDS = f"{PROJECT_NAME}:{Service.DATA.value}:all_mids"
+    ALL_MIDS_COUNT = f"{PROJECT_NAME}:all_mids_count"
+    PERP_CANDLE = f"{PROJECT_NAME}:{Service.DATA.value}:perp_candle"
+    PERP_MOMENTUM = f"{PROJECT_NAME}:{Service.DATA.value}:perp_momentum"
+    PERP_META = f"{PROJECT_NAME}:{Service.DATA.value}:perp_meta"
+    PERP_MARKET_DATA = f"{PROJECT_NAME}:{Service.DATA.value}:perp_market_data"
+    CANDLE = f"{PROJECT_NAME}:{Service.DATA.value}:candle"
+    MOMENTUM = f"{PROJECT_NAME}:{Service.DATA.value}:momentum"
+    MARKET_DATA = f"{PROJECT_NAME}:{Service.DATA.value}:market_data"
+
+
+class RebalanceRedisKey(Enum):
+    PNL_PER_TICKER = f"{PROJECT_NAME}:{Service.REBALANCE.value}:pnl:{{ticker}}"
+    PNL_TOTAL_PER_PUBLIC_KEY = (
+        f"{PROJECT_NAME}:{Service.REBALANCE.value}:pnl:total:{{public_key}}"
+    )
