ece13301a853e2c39f8b6464c3890fdf305d3c8e
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-03-11 12:15:00 +0900

    topic 추상화

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/kafka.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/kafka.py
@@ -1,30 +1,48 @@
 from enum import Enum
 from hypurrquant_fastapi_core.constant.projects import PROJECT_NAME, Service
+import os
+
+PROFILE = os.getenv("PROFILE", "prod")
 
 
 class AccountKafkaTopic(Enum):
-    DELETE_ACCOUNT = f"{PROJECT_NAME}.{Service.ACCOUNT.value}.account.delete"
+    DELETE_ACCOUNT = f"{PROJECT_NAME}_{Service.ACCOUNT.value}_account_delete"
     REBALANCE_ACCOUNT_CHANGE = (
-        f"{PROJECT_NAME}.{Service.ACCOUNT.value}.rebalance.account.change"
+        f"{PROJECT_NAME}_{Service.ACCOUNT.value}_rebalance_account_change"
     )
     REBALANCE_ACCOUNT_REFRESH = (
-        f"{PROJECT_NAME}.{Service.ACCOUNT.value}.rebalance.account.refresh"
+        f"{PROJECT_NAME}_{Service.ACCOUNT.value}_rebalance_account_refresh"
     )
     SPOT_BALANCE_REFRESH = (
-        f"{PROJECT_NAME}.{Service.ACCOUNT.value}.spot.balance.refresh"
+        f"{PROJECT_NAME}_{Service.ACCOUNT.value}_spot_balance_refresh"
     )
 
 
 class DataKafkaTopic(Enum):
     SPOT_MARKET_DATA_MID_PRICE = (
-        f"{PROJECT_NAME}.{Service.DATA.value}.spotMarket.midPrice"
+        f"{PROJECT_NAME}_{Service.DATA.value}_spotMarket_midPrice"
     )
 
 
 class RebalanceKafkaTopic(Enum):
     REBLANACE_ACCOUNT_DELETE = (
-        f"{PROJECT_NAME}.{Service.REBALANCE.value}.account.delete"
+        f"{PROJECT_NAME}_{Service.REBALANCE.value}_account_delete"
     )
     REBALANCE_ACCOUNT_REFRESH = (
-        f"{PROJECT_NAME}.{Service.REBALANCE.value}.account.refresh"
+        f"{PROJECT_NAME}_{Service.REBALANCE.value}_account_refresh"
     )
+
+
+def get_topic(default_topic: str) -> str:
+    """
+    env_var_name에 해당하는 환경 변수 값이 존재하면 그 값을 사용하고,
+    그렇지 않으면 default_topic을 반환합니다_
+    """
+
+    if PROFILE == "prod":
+        tmp = os.getenv(default_topic)
+        if not tmp:
+            raise ValueError(f"환경 변수 {default_topic}이 존재하지 않습니다_")
+        return tmp
+    else:
+        return default_topic
