aceb2ab10a54dfb1f162fc3959c9b9260c1be703
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-03-25 10:17:27 +0900

    feat: 긴급. market-data 스펙 변경

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/models/market_data.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/models/market_data.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel, Field
 from typing import Optional, Union, List
 
 
+class EvmContract(BaseModel):
+    address: str
+    evm_extra_wei_decimals: int
+
+
 class MarketData(BaseModel):
     prevDayPx: float
     dayNtlVlm: float
@@ -22,7 +27,7 @@ class MarketData(BaseModel):
     index_y: int
     tokenId: str
     isCanonical_y: bool
-    evmContract: Optional[str] = None
+    evmContract: Optional[EvmContract] = None
     fullName: Optional[str] = None  # full name은 있을 수도 있고 없을 수도 있음.
     MarketCap: float
     change_24h: float
@@ -30,18 +35,15 @@ class MarketData(BaseModel):
     sector: Optional[str] = None
 
     def __init__(self, **kwargs):
-        test_set = set()
-        try:
-            if "24hchange" in kwargs:
-                kwargs["change_24h"] = kwargs.pop("24hchange")
-            elif "change_24h" in kwargs:
-                kwargs["change_24h"] = kwargs.pop("change_24h")
+        # 키 이름 통일: "24hchange" 혹은 "24hchange_pct"를 변경
+        if "24hchange" in kwargs:
+            kwargs["change_24h"] = kwargs.pop("24hchange")
+        elif "change_24h" in kwargs:
+            kwargs["change_24h"] = kwargs.pop("change_24h")
 
-            if "24hchange_pct" in kwargs:
-                kwargs["change_24h_pct"] = kwargs.pop("24hchange_pct")
-            elif "change_24h_pct" in kwargs:
-                kwargs["change_24h_pct"] = kwargs.pop("change_24h_pct")
+        if "24hchange_pct" in kwargs:
+            kwargs["change_24h_pct"] = kwargs.pop("24hchange_pct")
+        elif "change_24h_pct" in kwargs:
+            kwargs["change_24h_pct"] = kwargs.pop("change_24h_pct")
 
-            super().__init__(**kwargs)
-        except KeyError:
-            print("MarketData._init__ KeyError")
+        super().__init__(**kwargs)
