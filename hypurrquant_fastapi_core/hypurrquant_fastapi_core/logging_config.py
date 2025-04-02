d99dde8ce744401c666fb71399dcdaf88a6c717f
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-04-02 12:46:57 +0900

    feat: 코루틴 로깅 추가

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
@@ -1,5 +1,19 @@
 import logging
 import os
+import contextvars
+import uuid
+import functools
+
+# contextvars를 사용하여 코루틴별 ID 저장
+coroutine_id = contextvars.ContextVar("coroutine_id", default="N/A")
+
+
+# 커스텀 로깅 필터
+class CoroutineFilter(logging.Filter):
+    def filter(self, record):
+        record.coroutine_id = coroutine_id.get()
+        return True
+
 
 logger_level = os.getenv("LOG_LEVEL", "DEBUG").upper()
 if isinstance(logger_level, int):
@@ -11,25 +25,33 @@ def configure_logging(file_path):
     로깅 설정 함수. 파일 이름에 따라 파일 핸들러가 동적으로 추가됩니다.
     :param file_path: 호출 파일 경로
     """
-    # 호출 파일 이름에서 파일명을 추출
-    # file_name = os.path.splitext(os.path.basename(file_path))[0]
-
-    # 로그 디렉토리 설정 및 생성 (필요시)
     log_dir = "logs"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir, exist_ok=True)
 
-    # 콘솔 핸들러 설정 (DEBUG 레벨만 처리)
+    # 콘솔 핸들러 설정 (DEBUG 레벨 이상 처리)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logger_level)  # DEBUG 이상만 콘솔에 출력
+    console_handler.setLevel(logger_level)
     console_formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s [PID: %(process)d, TID: %(thread)d FUNC: %(funcName)s, LINE: %(lineno)d]"
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s "
+        "[PID: %(process)d, TID: %(thread)d, FUNC: %(funcName)s, LINE: %(lineno)d COROUTINE_ID: %(coroutine_id)s]"
     )
     console_handler.setFormatter(console_formatter)
+    console_handler.addFilter(CoroutineFilter())
 
-    # 로깅 설정
-    logger = logging.getLogger(file_path)  # 호출 파일 이름을 로거 이름으로 사용
-    logger.setLevel(logger_level)  # 전체 로깅 레벨 설정
+    # 로거 설정 (호출 파일명을 로거 이름으로 사용)
+    logger = logging.getLogger(file_path)
+    logger.setLevel(logger_level)
     logger.addHandler(console_handler)
 
     return logger
+
+
+def coroutine_logging(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        # 함수 실행 전에 고유한 UUID를 할당
+        coroutine_id.set(str(uuid.uuid4()))
+        return await func(*args, **kwargs)
+
+    return wrapper
