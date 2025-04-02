731a1c19cd2bffd2a7061758dab3602a38c46710
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-04-02 18:48:08 +0900

    feat: formatting 변경

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
@@ -34,18 +34,18 @@ class SlackFormatter(logging.Formatter):
 
     def format(self, record):
         # 기본 날짜 및 시간, 로거 관련 정보를 포맷팅
-        basic_info = f"3. {self.formatTime(record, self.datefmt)} - {record.name} [PID: {record.process}, TID: {record.thread}, FUNC: {record.funcName}, LINE: {record.lineno}, COROUTINE_ID: {record.coroutine_id}]"
+        basic_info = f"3. Environment: {self.formatTime(record, self.datefmt)} - {record.name} [PID: {record.process}, TID: {record.thread}, FUNC: {record.funcName}, LINE: {record.lineno}, COROUTINE_ID: {record.coroutine_id}]\n"
         # 1. 서버 이름
-        server_info = f"1. 서버 이름: {self.server_name}"
+        server_info = f"1. Server name: {self.server_name}\n"
         # 2. 에러 무게 (레벨 이름과 번호)
-        error_weight = f"2. 에러 무게: {record.levelname} ({record.levelno})"
+        error_weight = f"2. Error Type: {record.levelname} ({record.levelno})\n"
         # 4. 메시지
-        message = f"4. 메시지: {record.getMessage()}"
+        message = f"4. Message: {record.getMessage()}\n"
         # 5. 예외 정보 (있다면 삼중 backticks로 감싸기)
         exception_info = ""
         if record.exc_info:
             exc_text = self.formatException(record.exc_info)
-            exception_info = f" | 5. 예외: ```{exc_text}```"
+            exception_info = f" | 5. Exception: ```{exc_text}```"
 
         # 각 항목을 "|" 기호로 한 줄에 모두 연결 (번호별 네이밍)
         formatted = (
