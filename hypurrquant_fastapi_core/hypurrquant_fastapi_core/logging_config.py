881add5dfb642675e49b969dfc54209353ca196e
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-04-02 12:23:58 +0900

    feat: 로깅 레벨 설정 추가

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/logging_config.py
@@ -1,6 +1,10 @@
 import logging
 import os
 
+logger_level = os.getenv("LOG_LEVEL", "DEBUG").upper()
+if isinstance(logger_level, int):
+    logger_level = logging.getLevelName(logger_level)
+
 
 def configure_logging(file_path):
     """
@@ -17,15 +21,15 @@ def configure_logging(file_path):
 
     # 콘솔 핸들러 설정 (DEBUG 레벨만 처리)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)  # DEBUG 이상만 콘솔에 출력
+    console_handler.setLevel(logger_level)  # DEBUG 이상만 콘솔에 출력
     console_formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s [PID: %(process)d, TID: %(thread)d]"
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s [PID: %(process)d, TID: %(thread)d FUNC: %(funcName)s, LINE: %(lineno)d]"
     )
     console_handler.setFormatter(console_formatter)
 
     # 로깅 설정
     logger = logging.getLogger(file_path)  # 호출 파일 이름을 로거 이름으로 사용
-    logger.setLevel(logging.DEBUG)  # 전체 로깅 레벨 설정
+    logger.setLevel(logger_level)  # 전체 로깅 레벨 설정
     logger.addHandler(console_handler)
 
     return logger
