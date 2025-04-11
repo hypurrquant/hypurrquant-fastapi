9bd520168c4ff17f819c62977b5061c533fb84b7
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-04-12 00:44:02 +0900

    41

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/exception.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/exception.py
@@ -373,6 +373,81 @@ class CopytradingServerException(BaseOrderException):  # 5000번대 에러
     pass
 
 
+class CannotAcquireLockException(CopytradingServerException):
+    """
+    구독 요청 처리 중 락을 획득하지 못한 경우 발생하는 예외입니다.
+    """
+
+    def __init__(self, message: str, api_response=None):
+        """
+        Args:
+            message (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(message, 5000, api_response)
+
+
+class SubscribeFailException(CopytradingServerException):
+    """
+    구독 요청 처리 중 에러가 발생한 경우 발생하는 예외입니다.
+    """
+
+    def __init__(self, message: str, api_response=None):
+        """
+        Args:
+            message (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(message, 5001, api_response)
+
+
+class NoSuchSubscriptionException(CopytradingServerException):
+    """
+    구독이 존재하지 않는 경우 발생하는 예외입니다.
+    """
+
+    def __init__(self, message: str, api_response=None):
+        """
+        Args:
+            message (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(message, 5002, api_response)
+
+
+class CannotDeleteSubscriptionException(CopytradingServerException):
+    """
+    구독 삭제 요청 처리 중 에러가 발생한 경우 발생하는 예외입니다.
+    """
+
+    def __init__(self, message: str, api_response=None):
+        """
+        Args:
+            message (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(message, 5003, api_response)
+
+
+class NotCopyTradingAccountException(CopytradingServerException):
+    """
+    CopyTrading 계좌가 아닌 경우 발생한다.
+    """
+
+    def __init__(self, response: str, api_response=None):
+        """
+        Args:
+            response (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(response, 5004, api_response)
+
+
 class StrategyServerException(BaseOrderException):  # 7000번대 에러
 
     pass
