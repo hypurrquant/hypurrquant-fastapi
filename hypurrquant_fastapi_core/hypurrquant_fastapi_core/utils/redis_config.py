8122a62e0adde67e50a4c8922b3cdd8ad545a2ba
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-04-18 16:36:58 +0900

    56

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/utils/redis_config.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/utils/redis_config.py
@@ -1,7 +1,14 @@
-import redis.asyncio as redis
+import os
+
+# 비동기용
+import redis.asyncio as aioredis
+import redis as sync_redis
 import os
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+redis_client = aioredis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+redis_client_sync = sync_redis.Redis(
+    host=REDIS_HOST, port=REDIS_PORT, decode_responses=True
+)
