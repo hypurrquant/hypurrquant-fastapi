da0afebe67bcf7e18224e5f6bd0b5f55f3d0ee9e
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-06-14 22:47:21 +0900

    d

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/exception.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/exception.py
@@ -693,6 +693,25 @@ class MaxSubscriptionReachedException(CopytradingServerException):
         super().__init__(response, 5008, api_response)
 
 
+class DcaServerException(BaseOrderException):  # 6000번대 에러
+    pass
+
+
+class DuplicationDcaException(DcaServerException):
+    """
+    DCA 주문이 중복된 경우 발생하는 예외입니다.
+    """
+
+    def __init__(self, message: str, api_response=None):
+        """
+        Args:
+            message (str): Error message from APIResponse.
+            code (int): Error code.
+            api_response (Optional[Any]): The APIResponse object.
+        """
+        super().__init__(message, 6000, api_response)
+
+
 class StrategyServerException(BaseOrderException):  # 7000번대 에러
 
     pass
