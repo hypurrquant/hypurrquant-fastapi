e0972a33b5d3526fbe29168efe67a6c37a8487ea
useongee <118250509+USEONGEE@users.noreply.github.com>
2025-07-07 22:28:39 +0900

    feat: HL api 공통

--- hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/projects.py
+++ hypurrquant_fastapi_core/hypurrquant_fastapi_core/constant/projects.py
@@ -1,6 +1,10 @@
 from enum import Enum
+from hyperliquid.utils.constants import MAINNET_API_URL
+import os
 
 PROJECT_NAME = "hypurrquant"
+
+HYPERLIQUID_API_URL = os.environ.get("HYPERLIQUID_API_URL", MAINNET_API_URL)
 
 
 class Service(Enum):
